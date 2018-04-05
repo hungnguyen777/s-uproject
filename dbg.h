9b52e6e37c499cdb64e88512b1e0dd6affb1919e
hungnguyen777 <36111855+hungnguyen777@users.noreply.github.com>
2018-04-04 21:05:36 -0500

    Update dbg.h

--- dbg.h
+++ dbg.h
@@ -1,3 +1,15 @@
+#ifndef __dbg_h__
+#define __dbg_h__
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#ifdef NDEBUG
+#define debug(M, ...)
+
+#else
+
 #define debug(M, ...)	fprintf(stderr, "DEBUG %s:%d: " M "\n",\
 		__FILE__, __LINE__, ##__VA_ARGS__)
 
